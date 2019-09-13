7b032ecf293dbef0f615c373ffc38e8acaa31a82
Fábio Lima <lr.fabioml@gmail.com>
2019-09-13 10:08:47 -0400

    var reassign redeclaration and scope

---
+++ 1.var-let-const/1.1.var-reassign-redeclaration.js
@@ -0,0 +1,13 @@
+var sample = 'value 1';
+
+console.log(sample);
+
+// // reassign
+// sample = 'value 2';
+
+// console.log(sample);
+
+// // redeclaration
+// var sample = 'value 3';
+
+// console.log(sample);
---
+++ 1.var-let-const/1.2.var.scope.js
@@ -0,0 +1,37 @@
+// function scope
+function sample() {
+  var abreu = 'sample 1';
+
+  console.log(abreu);
+}
+
+sample();
+
+///////////////////////////////
+
+// // function scope sample 2
+// function sample() {
+//   var abreu = 'sample 2';
+// }
+
+// sample()
+// console.log(abreu);
+
+
+///////////////////////////////
+
+// // block scope
+// if (true) {
+//   var abreu = 'sample 1';
+// }
+
+// console.log(abreu);
+
+///////////////////////////////
+
+// // block scope, sample 2
+// if (false) {
+//   var abreu = 'sample 2';
+// }
+
+// console.log(abreu);
