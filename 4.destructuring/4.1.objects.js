2183ba27051fc26308519b92d0403366f351c2e5
Fábio Lima <lr.fabioml@gmail.com>
2019-09-16 18:28:56 -0400

    destructuring with object and array

---
+++ 4.destructuring/4.1.objects.js
@@ -0,0 +1,25 @@
+const flima = {
+  name: 'Fábio',
+  country: 'Brazil',
+  twitter: '@fabiooml',
+};
+
+// const name = flima.name;
+// const country = flima.country;
+
+// const { name, country } = flima;
+
+// console.log(name);
+// console.log(country);
+
+
+//rename
+// const { name: nome } = flima;
+// console.log(nome);
+// console.log(name);
+
+//default valueS
+// const { name = 'Fabio', age } = flima;
+// const { name = 'Fabio', age: idade = 18 } = flima;
+// console.log(name);
+// console.log(idade);
---
+++ 4.destructuring/4.2.arrays.js
@@ -0,0 +1,12 @@
+const myArray = ['a', 'b', 'c', 'd', 'e'];
+
+// const [a, b, c, d, e] = myArray;
+// console.log(a, b, c, d, e);
+
+// const [a, b] = myArray;
+// console.log(a, b);
+
+// // rest operator
+// const [a, b, ...letters] = myArray;
+// console.log(a, b);
+// console.log(letters);
