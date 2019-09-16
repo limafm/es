31c7f021308f8dc76c1098bbb379b8a759184e98
Fábio Lima <lr.fabioml@gmail.com>
2019-09-16 17:51:12 -0400

    strings

---
+++ 3.strings/3.1.template-strings.js
@@ -0,0 +1,12 @@
+const pl1 = 'javascript';
+const pl2 = "php";
+const randomNumber = 15;
+
+const example = pl1 + ' is better than ' + pl2;
+
+// const example = `${pl1} is batter than ${pl2}`;
+
+// let example = `Random number: ${randomNumber}.`
+// example += `\nRandom number * 10: ${randomNumber * 10}`;
+
+console.log(example);
---
+++ 3.strings/3.2.string-methods.js
@@ -0,0 +1,18 @@
+const test = 'asdf_34_HFG_56';
+
+//.startsWith
+console.log(test.startsWith('asdf'));
+// console.log(test.startsWith('ASDF'));
+// console.log(test.startsWith('34', 5));
+
+//.endsWith
+// console.log(test.endsWith('56'));
+// console.log(test.endsWith('HFG', 11));
+
+//.includes
+// console.log(test.includes('34'));
+// console.log(test.includes('343'));
+
+//.repeat
+// console.log('ABC'.repeat(10));
+// console.log(`${'x' * 5}`.repeat(10) + ' Batman!');
