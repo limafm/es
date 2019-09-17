d1c063b9aff7e334d44411dc01b17ea004357593
Fábio Lima <lr.fabioml@gmail.com>
2019-09-17 14:14:20 -0400

    for of loop

---
+++ 5.for-of-loop/5.1.sample.js
@@ -0,0 +1,33 @@
+// Array.prototype.example = 'example';
+
+const frutas = ['abacaxi', 'abacate', 'cupuaçu', 'uva'];
+
+// for (let i = 0; i < frutas.length; i++) {
+//   console.log(frutas[i]);
+// }
+
+// frutas.forEach(function(fruta) {
+//   console.log(fruta);
+
+//   // if (fruta === 'abacate') {
+//   //   break;
+//   // }
+// });
+
+// frutas.some(function(fruta) {
+//   console.log(fruta);
+
+//   return fruta === 'abacate';
+// });
+
+// for (const i in frutas) {
+//   console.log(frutas[i]);
+// }
+
+// for (const fruta of frutas) {
+//   console.log(fruta);
+
+//   // if (fruta === 'abacaxi') {
+//   //   break;
+//   // }
+// }
---
+++ 5.for-of-loop/5.2.sample2.js
@@ -0,0 +1,46 @@
+const frutas = ['abacaxi', 'abacate', 'cupuaçu', 'uva'];
+
+// console.log(frutas.entries());
+
+// // examples
+// const it = frutas.entries();
+// console.log(it.next());
+// // console.log(it.next());
+// // console.log(it.next());
+// // console.log(it.next());
+// // console.log(it.next());
+
+////////////////////////////////////////
+
+// for (const [index, value] of frutas.entries()) {
+//   console.log(`index: ${index}, value: ${value}`);
+// }
+
+////////////////////////////////////////
+
+// Objects
+// const randomObject = {
+//   um: '1 um',
+//   dois: '2 dois',
+//   tres: '3 tres',
+// };
+
+// for (const property in randomObject) {
+//   const value = randomObject[property];
+
+//   console.log('property:', property);
+//   console.log('value:', value);
+// }
+
+// for (const property of Object.keys(randomObject)) {
+//   const value = randomObject[property];
+
+//   console.log('property:', property);
+//   console.log('value:', value);
+// }
+
+// // es2017 / node 7
+// for (const [property, value] of Object.entries(randomObject)) {
+//   console.log('property:', property);
+//   console.log('value:', value);
+// }
